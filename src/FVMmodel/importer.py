5641a236ef5b206e6f56670fe9f3bae8aaf1211d
Hamza Kamil <161087997+Hamza-Kamil@users.noreply.github.com>
2025-04-17 17:48:46 +0100

    Utils to utils.py
    Fix: Changed Utils to utils for case-sensitive imports

--- src/FVMmodel/importer.py
+++ src/FVMmodel/importer.py
@@ -2,8 +2,8 @@ import torch
 import numpy as np
 import torch.nn as nn
 from torch_scatter import scatter_mean,scatter_min,scatter_max
-from Utils.utilities import NodeType
-from Utils.normalization import Normalizer
+from utils.utilities import NodeType
+from utils.normalization import Normalizer
 from FVMmodel.FVdiscretization.FVscheme import Intergrator
 from timm.layers import trunc_normal_
 
